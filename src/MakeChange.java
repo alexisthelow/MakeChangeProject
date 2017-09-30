bc202ae701c8ae717285b3d06af7b6e37da73d96
Alexis Low <alexisthelow@gmail.com>
2017-09-30 11:22:00 -0600

    added do-while loop for tendered entry, closed scanner at end

--- src/MakeChange.java
+++ src/MakeChange.java
@@ -17,10 +17,22 @@ public class MakeChange {
 				System.out.println("You didn't enter a valid amount. Please try again.");
 			}
 		} while (!priceEntry); //keep going until priceFlag is true
-			//ask how much money was tendered by customer
+		//ask how much money was tendered by customer
+		boolean paidEntry = false;
+		do {
+			System.out.print("Please enter the amount tendered by the customer: ");
+			if (scanner.hasNextDouble()) { // check to make sure it's a double
+				double paid = scanner.nextDouble(); // if so, put it in the paid variable
+				paidEntry = true; //and set the paid flag to true
+			}
+			else {
+				scanner.next(); // if not, clear the scanner and tell the user they messed up
+				System.out.println("You didn't enter a valid amount. Please try again.");
+			}
+		} while (!paidEntry); //keep going until priceFlag is true
 		//display message if tendered was less than or equal to price
 		//display number of bills and coins in change if tendered was greater than price
-		
+		scanner.close();
 	}
 
 }
