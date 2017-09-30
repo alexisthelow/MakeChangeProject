8f9a8d02050e251edeed3ef531b73e4fb3bd1ae1
Alexis Low <alexisthelow@gmail.com>
2017-09-30 11:10:06 -0600

    added do-while loop for price entry

--- src/MakeChange.java
+++ src/MakeChange.java
@@ -5,15 +5,19 @@ public class MakeChange {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		//ask user for price of item
-		System.out.print("Please enter the price of the item: ");
-		if (scanner.hasNextDouble()) { // check to make sure it's a double
-			double price = scanner.nextDouble(); // if so, put it in the price variable
-		}
-		else {
-			scanner.next(); // if not, clear the scanner and tell the user they messed up
-			System.out.println("You didn't enter a valid amount. Please try again.");
-		}
-		//ask how much money was tendered by customer
+		boolean priceEntry = false;
+		do {
+			System.out.print("Please enter the price of the item: ");
+			if (scanner.hasNextDouble()) { // check to make sure it's a double
+				double price = scanner.nextDouble(); // if so, put it in the price variable
+				priceEntry = true; //and set the priceFlag to true
+			}
+			else {
+				scanner.next(); // if not, clear the scanner and tell the user they messed up
+				System.out.println("You didn't enter a valid amount. Please try again.");
+			}
+		} while (!priceEntry); //keep going until priceFlag is true
+			//ask how much money was tendered by customer
 		//display message if tendered was less than or equal to price
 		//display number of bills and coins in change if tendered was greater than price
 		
