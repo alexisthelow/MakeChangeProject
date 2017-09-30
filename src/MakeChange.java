a4ab5ef8739a9da6826dd5bf86ec338dc68fc8d3
Alexis Low <alexisthelow@gmail.com>
2017-09-30 11:07:40 -0600

    added ask for price and check validation of entry

---
+++ src/MakeChange.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class MakeChange {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		//ask user for price of item
+		System.out.print("Please enter the price of the item: ");
+		if (scanner.hasNextDouble()) { // check to make sure it's a double
+			double price = scanner.nextDouble(); // if so, put it in the price variable
+		}
+		else {
+			scanner.next(); // if not, clear the scanner and tell the user they messed up
+			System.out.println("You didn't enter a valid amount. Please try again.");
+		}
+		//ask how much money was tendered by customer
+		//display message if tendered was less than or equal to price
+		//display number of bills and coins in change if tendered was greater than price
+		
+	}
+
+}
